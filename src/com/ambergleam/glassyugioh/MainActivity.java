4692deec2bb6a57d1e341ec2a331d00b9146b7b3
Ambergleam <ambergleam@gmail.com>
2013-11-27 14:25:08 -0500

    Updated layout, did some configuration, cleaned it up

--- src/com/ambergleam/glassyugioh/MainActivity.java
+++ src/com/ambergleam/glassyugioh/MainActivity.java
@@ -1,17 +1,7 @@
 package com.ambergleam.glassyugioh;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Random;
-
-import com.jzplusplus.glassroll.R;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.graphics.Color;
-import android.speech.RecognizerIntent;
-import android.view.KeyEvent;
+import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -22,43 +12,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		((TextView) findViewById(R.id.userLife)).setText(Integer.toString(value));
+	}
+
+	/*
+	 * Updates the screen
+	 */
+	private void updateState() {
+		((TextView) findViewById(R.id.userLife)).setText(Integer.toString(userLife));
+		((TextView) findViewById(R.id.enemyLife)).setText(Integer.toString(enemyLife));
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-
-		// Get the user voice input
-		Bundle b = getIntent().getExtras();
-		if (b != null) {
-			ArrayList<String> voiceResults = getIntent().getExtras().getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
-			// Parse it into a number
-			NumberFormat nf = NumberFormat.getIntegerInstance();
-			nf.setParseIntegerOnly(true);
-			try {
-				// Convert to a number
-				String voiceResultsString = voiceResults.get(0).substring(1);
-				dieValue = nf.parse(voiceResultsString).intValue();
-			} catch (ParseException e) {
-				// Didn't properly convert so exit out
-				finish();
-			}
-		}
-		rollIt();
-	}
-
-	/*
-	 * On key pad tap, the app rolls again
-	 */
-	@Override
-	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		super.onKeyUp(keyCode, event);
-		if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-			rollIt();
-			return true;
-		}
-		return false;
+		updateState();
 	}
 
 }
